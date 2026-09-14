d938c8db2c15160049f4951c42fe6ac18b5adbc6
yg <455928551@qq.com>
2026-09-14 15:48:20 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => ({
-  base: './',
+   base: mode === 'production' ? '/blockMedicine/' : '/',
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
